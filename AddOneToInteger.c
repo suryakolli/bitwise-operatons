1e08c13f4a784d85d12d4f13aeea152f366d4269
Surya Kolli <chandrasekhar.kohli@gmail.com>
2022-09-30 13:55:43 -0400

    Create AddOneToInteger.c

---
+++ AddOneToInteger.c
@@ -0,0 +1,17 @@
+// add one to given integer n
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int x = 4;
+    cout << x << " + " << 1 << " is " << -~x << endl;
+ 
+    x = -5;
+    cout << x << " + " << 1 << " is " << -~x << endl;
+ 
+    x = 0;
+    cout << x << " + " << 1 << " is " << -~x << endl;
+ 
+    return 0;
+}
