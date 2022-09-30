dce4c6ed13523b531dae71e10d4d52de15b3d1a3
Surya Kolli <chandrasekhar.kohli@gmail.com>
2022-09-30 13:52:40 -0400

    Create CheckNisEvenOrOdd.c

---
+++ CheckNisEvenOrOdd.c
@@ -0,0 +1,17 @@
+// check if a number is even or odd
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int n = 5;
+ 
+    if (n & 1) {
+        cout << n << " is odd";
+    }
+    else {
+        cout << n << " is even";
+    }
+ 
+    return 0;
+}
